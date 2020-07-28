fa173a6f60e56b4f996c451b9119e9e068ee4d93
thiagohrcosta <thiagohrcosta86@gmail.com>
2020-07-28 10:55:18 -0300

    Criando primeiro componente

--- movie-trailer/src/App.js
+++ movie-trailer/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Menu from './components/Menu';
 
 function App() {
   return (
     <div>
+      <Menu />
       <h1>Hello World</h1>
       Imersão Alura
     </div>
---
+++ movie-trailer/src/components/Menu/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Menu(){
+  return (
+  <header>
+    MovieFlix
+  </header>
+  )
+}
+
+export default Menu;
