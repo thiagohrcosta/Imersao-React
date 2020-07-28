b71ee560712747a1aa4ea4b53ce77503e6638419
thiagohrcosta <thiagohrcosta86@gmail.com>
2020-07-28 14:07:43 -0300

    Adicionando conteúdo na página principal

--- movie-trailer/src/App.js
+++ movie-trailer/src/App.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import Menu from './components/Menu';
+import Carousel from './components/Carousel';
+import BannerMain from './components/BannerMain';
 
+import dadosIniciais from './data/dados_iniciais.json';
 function App() {
   return (
-    <div>
+    <div style={{background: "#141414"}}>
       <Menu />
+      <BannerMain
+        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+        url={dadosIniciais.categorias[0].videos[0].url}
+        videoDescription={"Um veterano da Guerra do Vietnã usa todo seu treinamento e agressividade exercitada nos campos de batalha quando é preso e torturado por policiais."}        
+      />
+
+      <Carousel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[0]}
+      />
+
+      <Carousel
+        category={dadosIniciais.categorias[1]}
+      />
     </div>
   );
 }
