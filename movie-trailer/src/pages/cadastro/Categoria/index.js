ea16a7e3707d8942cbac2c60b9912db46256bcc0
thiagohrcosta <thiagohrcosta86@gmail.com>
2020-07-30 22:03:22 -0300

    Iniciando Formulários e useState

--- movie-trailer/src/pages/cadastro/Categoria/index.js
+++ movie-trailer/src/pages/cadastro/Categoria/index.js
@@ -3,12 +3,19 @@ import PageDefault from '../../../components/PageDefault';
 import {Link} from 'react-router-dom';
 
 function CadastroCategoria(){
+
+  useState('Filmes');
+
   return (
     <PageDefault>
-    <h1>Cadastro de Categoria</h1>
+    <h1>Cadastro de Categoria: {nomeDaCategoria}</h1>
 
     <form action="">
-      
+      {/* State */}
+      <label htmlFor="">
+        Nome da Categoria:
+        <input type="text" />
+      </label>
     </form>
       <Link to="/">
         Ir para home
